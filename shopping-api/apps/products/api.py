c3b3ff4e7dbd2ac6cbeaf9db9f0558560681beca
thiagolopes <thiagolopes@protonmail.com>
2020-12-18 17:29:49 -0300

    add ProducViewSet

---
+++ shopping-api/apps/products/api.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from apps.products.models import ProductModel
+from apps.products.serializers import ProductDiscoutSerializer, ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    http_method_names = [
+        "get",
+        "post",
+        "delete",
+        "head",
+        "options",
+    ]
+
+    queryset = ProductModel.objects.all()
+    serializer_class = ProductSerializer
+    serializer_class_authenticated = ProductDiscoutSerializer
+
+    def get_serializer_class(self):
+        if self.request.user.is_authenticated:
+            return self.serializer_class_authenticated
+        return self.serializer_class
