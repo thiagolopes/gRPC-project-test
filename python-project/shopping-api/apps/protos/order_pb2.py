4b9301a4c963f9eb9662d113d2ac6bf686773feb
thiagolopes <thiagolopes@protonmail.com>
2021-01-06 18:20:46 -0300

    regenerate proto calls

--- python-project/shopping-api/apps/protos/order_pb2.py
+++ python-project/shopping-api/apps/protos/order_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x0border.proto\x12\x05order\"&\n\x04User\x12\n\n\x02id\x18\x01 \x01(\t\x12\x12\n\ndate_birth\x18\x02 \x01(\x03\"$\n\x07Product\x12\n\n\x02id\x18\x01 \x01(\t\x12\r\n\x05price\x18\x02 \x01(\x02\"0\n\x08\x44iscount\x12\x12\n\npercentage\x18\x01 \x01(\x02\x12\x10\n\x08\x64iscount\x18\x02 \x01(\x02\"D\n\x05Order\x12 \n\x08products\x18\x01 \x03(\x0b\x32\x0e.order.Product\x12\x19\n\x04user\x18\x02 \x01(\x0b\x32\x0b.order.User\"t\n\x11OrderWithDiscount\x1a_\n\x07Product\x12\x31\n\x07product\x18\x01 \x01(\x0b\x32 .order.OrderWithDiscount.Product\x12!\n\x08\x64iscount\x18\x02 \x01(\x0b\x32\x0f.order.Discount2S\n\x0f\x44iscountService\x12@\n\x16\x43\x61lculateOrderDiscount\x12\x0c.order.Order\x1a\x18.order.OrderWithDiscountb\x06proto3'
+  serialized_pb=b'\n\x0border.proto\x12\x05order\"\x1a\n\x04User\x12\x12\n\ndate_birth\x18\x01 \x01(\x03\"\"\n\x05Order\x12\x19\n\x04user\x18\x01 \x01(\x0b\x32\x0b.order.User\"5\n\x08\x44iscount\x12\x12\n\npercentage\x18\x01 \x01(\x02\x12\x15\n\rdiscount_name\x18\x02 \x01(\t\"/\n\tDiscounts\x12\"\n\tdiscounts\x18\x01 \x03(\x0b\x32\x0f.order.Discount2G\n\x0f\x44iscountService\x12\x34\n\x12\x41vailableDiscounts\x12\x0c.order.Order\x1a\x10.order.Discountsb\x06proto3'
 )
 
 
@@ -34,15 +34,8 @@ _USER = _descriptor.Descriptor(
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='id', full_name='order.User.id', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='date_birth', full_name='order.User.date_birth', index=1,
-      number=2, type=3, cpp_type=2, label=1,
+      name='date_birth', full_name='order.User.date_birth', index=0,
+      number=1, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -60,29 +53,22 @@ _USER = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=22,
-  serialized_end=60,
+  serialized_end=48,
 )
 
 
-_PRODUCT = _descriptor.Descriptor(
-  name='Product',
-  full_name='order.Product',
+_ORDER = _descriptor.Descriptor(
+  name='Order',
+  full_name='order.Order',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='id', full_name='order.Product.id', index=0,
-      number=1, type=9, cpp_type=9, label=1,
-      has_default_value=False, default_value=b"".decode('utf-8'),
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='price', full_name='order.Product.price', index=1,
-      number=2, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
+      name='user', full_name='order.Order.user', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
@@ -98,8 +84,8 @@ _PRODUCT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=62,
-  serialized_end=98,
+  serialized_start=50,
+  serialized_end=84,
 )
 
 
@@ -119,9 +105,9 @@ _DISCOUNT = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='discount', full_name='order.Discount.discount', index=1,
-      number=2, type=2, cpp_type=6, label=1,
-      has_default_value=False, default_value=float(0),
+      name='discount_name', full_name='order.Discount.discount_name', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
@@ -137,33 +123,26 @@ _DISCOUNT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=100,
-  serialized_end=148,
+  serialized_start=86,
+  serialized_end=139,
 )
 
 
-_ORDER = _descriptor.Descriptor(
-  name='Order',
-  full_name='order.Order',
+_DISCOUNTS = _descriptor.Descriptor(
+  name='Discounts',
+  full_name='order.Discounts',
   filename=None,
   file=DESCRIPTOR,
   containing_type=None,
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='products', full_name='order.Order.products', index=0,
+      name='discounts', full_name='order.Discounts.discounts', index=0,
       number=1, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='user', full_name='order.Order.user', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -176,83 +155,16 @@ _ORDER = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=150,
-  serialized_end=218,
+  serialized_start=141,
+  serialized_end=188,
 )
 
-
-_ORDERWITHDISCOUNT_PRODUCT = _descriptor.Descriptor(
-  name='Product',
-  full_name='order.OrderWithDiscount.Product',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-    _descriptor.FieldDescriptor(
-      name='product', full_name='order.OrderWithDiscount.Product.product', index=0,
-      number=1, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='discount', full_name='order.OrderWithDiscount.Product.discount', index=1,
-      number=2, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-  ],
-  extensions=[
-  ],
-  nested_types=[],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=241,
-  serialized_end=336,
-)
-
-_ORDERWITHDISCOUNT = _descriptor.Descriptor(
-  name='OrderWithDiscount',
-  full_name='order.OrderWithDiscount',
-  filename=None,
-  file=DESCRIPTOR,
-  containing_type=None,
-  create_key=_descriptor._internal_create_key,
-  fields=[
-  ],
-  extensions=[
-  ],
-  nested_types=[_ORDERWITHDISCOUNT_PRODUCT, ],
-  enum_types=[
-  ],
-  serialized_options=None,
-  is_extendable=False,
-  syntax='proto3',
-  extension_ranges=[],
-  oneofs=[
-  ],
-  serialized_start=220,
-  serialized_end=336,
-)
-
-_ORDER.fields_by_name['products'].message_type = _PRODUCT
 _ORDER.fields_by_name['user'].message_type = _USER
-_ORDERWITHDISCOUNT_PRODUCT.fields_by_name['product'].message_type = _ORDERWITHDISCOUNT_PRODUCT
-_ORDERWITHDISCOUNT_PRODUCT.fields_by_name['discount'].message_type = _DISCOUNT
-_ORDERWITHDISCOUNT_PRODUCT.containing_type = _ORDERWITHDISCOUNT
+_DISCOUNTS.fields_by_name['discounts'].message_type = _DISCOUNT
 DESCRIPTOR.message_types_by_name['User'] = _USER
-DESCRIPTOR.message_types_by_name['Product'] = _PRODUCT
-DESCRIPTOR.message_types_by_name['Discount'] = _DISCOUNT
 DESCRIPTOR.message_types_by_name['Order'] = _ORDER
-DESCRIPTOR.message_types_by_name['OrderWithDiscount'] = _ORDERWITHDISCOUNT
+DESCRIPTOR.message_types_by_name['Discount'] = _DISCOUNT
+DESCRIPTOR.message_types_by_name['Discounts'] = _DISCOUNTS
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 User = _reflection.GeneratedProtocolMessageType('User', (_message.Message,), {
@@ -262,12 +174,12 @@ User = _reflection.GeneratedProtocolMessageType('User', (_message.Message,), {
   })
 _sym_db.RegisterMessage(User)
 
-Product = _reflection.GeneratedProtocolMessageType('Product', (_message.Message,), {
-  'DESCRIPTOR' : _PRODUCT,
+Order = _reflection.GeneratedProtocolMessageType('Order', (_message.Message,), {
+  'DESCRIPTOR' : _ORDER,
   '__module__' : 'order_pb2'
-  # @@protoc_insertion_point(class_scope:order.Product)
+  # @@protoc_insertion_point(class_scope:order.Order)
   })
-_sym_db.RegisterMessage(Product)
+_sym_db.RegisterMessage(Order)
 
 Discount = _reflection.GeneratedProtocolMessageType('Discount', (_message.Message,), {
   'DESCRIPTOR' : _DISCOUNT,
@@ -276,27 +188,12 @@ Discount = _reflection.GeneratedProtocolMessageType('Discount', (_message.Messag
   })
 _sym_db.RegisterMessage(Discount)
 
-Order = _reflection.GeneratedProtocolMessageType('Order', (_message.Message,), {
-  'DESCRIPTOR' : _ORDER,
+Discounts = _reflection.GeneratedProtocolMessageType('Discounts', (_message.Message,), {
+  'DESCRIPTOR' : _DISCOUNTS,
   '__module__' : 'order_pb2'
-  # @@protoc_insertion_point(class_scope:order.Order)
+  # @@protoc_insertion_point(class_scope:order.Discounts)
   })
-_sym_db.RegisterMessage(Order)
-
-OrderWithDiscount = _reflection.GeneratedProtocolMessageType('OrderWithDiscount', (_message.Message,), {
-
-  'Product' : _reflection.GeneratedProtocolMessageType('Product', (_message.Message,), {
-    'DESCRIPTOR' : _ORDERWITHDISCOUNT_PRODUCT,
-    '__module__' : 'order_pb2'
-    # @@protoc_insertion_point(class_scope:order.OrderWithDiscount.Product)
-    })
-  ,
-  'DESCRIPTOR' : _ORDERWITHDISCOUNT,
-  '__module__' : 'order_pb2'
-  # @@protoc_insertion_point(class_scope:order.OrderWithDiscount)
-  })
-_sym_db.RegisterMessage(OrderWithDiscount)
-_sym_db.RegisterMessage(OrderWithDiscount.Product)
+_sym_db.RegisterMessage(Discounts)
 
 
 
@@ -307,16 +204,16 @@ _DISCOUNTSERVICE = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=338,
-  serialized_end=421,
+  serialized_start=190,
+  serialized_end=261,
   methods=[
   _descriptor.MethodDescriptor(
-    name='CalculateOrderDiscount',
-    full_name='order.DiscountService.CalculateOrderDiscount',
+    name='AvailableDiscounts',
+    full_name='order.DiscountService.AvailableDiscounts',
     index=0,
     containing_service=None,
     input_type=_ORDER,
-    output_type=_ORDERWITHDISCOUNT,
+    output_type=_DISCOUNTS,
     serialized_options=None,
     create_key=_descriptor._internal_create_key,
   ),
