ee6bd2f6a8af2c4f855cce71a702d6db12f0ae63
thiagolopes <thiagolopes@protonmail.com>
2021-01-24 16:35:44 -0300

    remove log

--- go-project/internal/promotion/promotion.go
+++ go-project/internal/promotion/promotion.go
@@ -10,7 +10,6 @@ type Server struct {
 }
 
 func (s *Server) AvailableDiscounts(ctx context.Context, order *Order) (*Discounts, error) {
-	log.Println(order)
 	log.Println(order.User.GetDateBirth())
 	return &Discounts{}, nil
 }
