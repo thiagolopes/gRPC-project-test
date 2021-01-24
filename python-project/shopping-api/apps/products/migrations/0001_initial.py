432491e6f326dac97b1282e22b961021865ebab7
thiagolopes <thiagolopes@protonmail.com>
2021-01-24 01:18:33 -0300

    run lint in migration

--- python-project/shopping-api/apps/products/migrations/0001_initial.py
+++ python-project/shopping-api/apps/products/migrations/0001_initial.py
@@ -1,27 +1,30 @@
-from django.db import migrations, models
 import uuid
+
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='ProductModel',
+            name="ProductModel",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('title', models.CharField(help_text='Product Title', max_length=64, unique=True)),
-                ('description', models.TextField(default='', help_text='Product description')),
-                ('price', models.DecimalField(decimal_places=2, help_text='Product price', max_digits=8)),
-                ('currency', models.CharField(choices=[('BRL', 'BRL'), ('USD', 'USD')], max_length=3)),
+                (
+                    "id",
+                    models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
+                ),
+                ("title", models.CharField(help_text="Product Title", max_length=64, unique=True)),
+                ("description", models.TextField(default="", help_text="Product description")),
+                ("price", models.DecimalField(decimal_places=2, help_text="Product price", max_digits=8)),
+                ("currency", models.CharField(choices=[("BRL", "BRL"), ("USD", "USD")], max_length=3)),
             ],
             options={
-                'verbose_name': 'Product',
-                'verbose_name_plural': 'Products',
+                "verbose_name": "Product",
+                "verbose_name_plural": "Products",
             },
         ),
     ]
