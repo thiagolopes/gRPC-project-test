9c1de1e78bd16412765e4da3b4c4f8d4f613eb4d
thiagolopes <thiagolopes@protonmail.com>
2021-01-25 02:33:21 -0300

    update test and add BirthDayPromotion tests

--- go-project/pkg/promotion/discounts_test.go
+++ go-project/pkg/promotion/discounts_test.go
@@ -1,31 +1,81 @@
 package promotion
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestBlackFridayPromotion(t *testing.T){
+func TestIsEqual(t *testing.T) {
+	Today := time.Now().UTC()
+	Yesterday := time.Now().Add(-24 * time.Hour)
+
+	if isEqual(Today, Today, "2006-01-02") != true {
+		t.Errorf("isEqual, expected=%v", true)
+	}
+
+	if isEqual(Today, Yesterday, "2006-01-02") != false {
+		t.Errorf("isEqual, expected=%v", false)
+	}
+}
+
+func TestBlackFridayPromotion(t *testing.T) {
 	tests := []struct {
-		Order Order
+		Order            Order
 		ExpectedDiscount Discount
 	}{
 		{
-			Order: Order{User: User{Date: "invalid"}},
-			ExpectedDiscount: Discount{Description: "user_date_parse_error"},
+			Order:            Order{User: User{Date: "invalid"}},
+			ExpectedDiscount: Discount{Description: "bf_promotion_user_date_parse_error"},
 		},
 		{
-			Order: Order{User: User{Date: "1995-11-26"}},
+			Order: Order{User: User{Date: "1995-11-25"}},
 			ExpectedDiscount: Discount{
 				Percentage:  BF_PERCENTAGE_DISCOUNT,
 				Description: "Black friday day promotion",
 			},
 		},
 		{
-			Order: Order{User: User{Date: "1995-11-24"}},
+			Order:            Order{User: User{Date: "1995-11-22"}},
 			ExpectedDiscount: Discount{},
 		},
 	}
 
 	for _, test := range tests {
 		discount := BlackFridayPromotion(test.Order)
+
+		if discount != test.ExpectedDiscount {
+			t.Errorf("BlackFridayPromotion, order=%v, expected=%v, returned=%v", test.Order, test.ExpectedDiscount, discount)
+		}
+	}
+}
+
+func TestBirthDayPromotion(t *testing.T) {
+	Today := time.Now().UTC().Format("2006-01-02")
+	Yesterday := time.Now().Add(-24 * time.Hour).Format("2006-01-02")
+
+	tests := []struct {
+		Order            Order
+		ExpectedDiscount Discount
+	}{
+		{
+			Order:            Order{User: User{Date: "invalid"}},
+			ExpectedDiscount: Discount{Description: "birth_day_promotion_user_date_parse_error"},
+		},
+		{
+			Order: Order{User: User{Date: DateISO(Today)}},
+			ExpectedDiscount: Discount{
+				Percentage:  BIRTH_DAY_PERCENTAGE_DISCOUNT,
+				Description: "Cake day promotion",
+			},
+		},
+		{
+			Order:            Order{User: User{Date: DateISO(Yesterday)}},
+			ExpectedDiscount: Discount{},
+		},
+	}
+
+	for _, test := range tests {
+		discount := BirthDayPromotion(test.Order)
 
 		if discount != test.ExpectedDiscount {
 			t.Errorf("BlackFridayPromotion, order=%v, expected=%v, returned=%v", test.Order, test.ExpectedDiscount, discount)
