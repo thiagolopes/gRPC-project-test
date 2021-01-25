b0c8904a91d5a4ad4e1a211a93ec7ca44a28a4aa
thiagolopes <thiagolopes@protonmail.com>
2021-01-25 02:31:00 -0300

    birth_day_percentage_discount in const, add isEqual func to time.Time and update some discount descriptions

--- go-project/pkg/promotion/discounts.go
+++ go-project/pkg/promotion/discounts.go
@@ -7,15 +7,23 @@ import (
 
 // TODO add in settings
 const (
-	BF_DAY                 = "25"
-	BF_MOUTH               = "11"
-	BF_TIME_PARSE          = "01-02"
-	BF_PERCENTAGE_DISCOUNT = 0.10
-	USER_TIME_PARSE        = "2006-01-02"
+	BF_DAY                        = "25"
+	BF_MOUTH                      = "11"
+	BF_TIME_PARSE                 = "01-02"
+	BF_PERCENTAGE_DISCOUNT        = 0.10
+	BIRTH_DAY_PERCENTAGE_DISCOUNT = 0.05
+	USER_TIME_PARSE               = "2006-01-02"
 )
 
 type Promotions struct {
 	Promotion []func(Order) Discount
+}
+
+func isEqual(t1, t2 time.Time, parse string) bool {
+	if t1.Format(parse) == t2.Format(parse) {
+		return true
+	}
+	return false
 }
 
 // BlackFridayPromotion is the function that describe the discounts in
@@ -24,16 +32,16 @@ func BlackFridayPromotion(order Order) Discount {
 	BFDate, err := time.Parse(BF_TIME_PARSE, fmt.Sprint(BF_MOUTH, "-", BF_DAY))
 	if err != nil {
 		fmt.Errorf("bf_date_parse_error, bf_date=%v, bf_parse=%v, error=%v", fmt.Sprint(BF_MOUTH, "-", BF_DAY), BF_TIME_PARSE, err)
-		return Discount{Description: "bf_date_parse_error"}
+		return Discount{Description: "bf_promotion_date_parse_error"}
 	}
 
 	UserDate, err := time.Parse(USER_TIME_PARSE, string(order.User.Date))
 	if err != nil {
 		fmt.Errorf("user_date_parse_error, user_date=%v, user_parse=%v, error=%v", order.User.Date, BF_TIME_PARSE, err)
-		return Discount{Description: "user_date_parse_error"}
+		return Discount{Description: "bf_promotion_user_date_parse_error"}
 	}
 
-	if UserDate.Format(BF_TIME_PARSE) == BFDate.Format(BF_TIME_PARSE){
+	if isEqual(UserDate, BFDate, BF_TIME_PARSE) == true {
 		return Discount{
 			Percentage:  BF_PERCENTAGE_DISCOUNT,
 			Description: "Black friday day promotion",
