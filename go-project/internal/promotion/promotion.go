902cbae5937c5c664740c499f8b84648ba1839cc
thiagolopes <thiagolopes@protonmail.com>
2021-01-28 22:39:48 -0300

    add availableDiscountsHandler in AvalibleDiscounts

--- go-project/internal/promotion/promotion.go
+++ go-project/internal/promotion/promotion.go
@@ -10,6 +10,14 @@ type Server struct {
 }
 
 func (s *Server) AvailableDiscounts(ctx context.Context, order *Order) (*Discounts, error) {
-	log.Println(order.User.GetDateBirth())
-	return &Discounts{}, nil
+	log.Printf("available_discounts_handler_start, order=%v, ctx=%v", order, ctx)
+	discounts := availableDiscountsHandler(order)
+	log.Printf("available_discounts_handler_end, discounts=%v", discounts)
+	return discounts, nil
+}
+
+func availableDiscountsHandler(o *Order) *Discounts {
+	p := promotion.AllPromotionsAvalibe()
+	d := promotion.VerifyDiscountsAvalibe(Order2Order(o), p)
+	return Discount2Discounts(d)
 }
